389ba4ebec6f19e50d64a54f0e025a4ac530bb47
shaozhixing <shaozhixing@shouqiev.com>
2020-07-11 16:07:16 +0800

    jdk动态代理

--- src/main/java/xx/ms/design/partern/demo/proxy/JdkProxy/JdkProxyMain.java
+++ src/main/java/xx/ms/design/partern/demo/proxy/JdkProxy/JdkProxyMain.java
@@ -1,5 +1,9 @@
 package xx.ms.design.partern.demo.proxy.JdkProxy;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+
 /**
  * @author: shaozhixing
  * @date: 2020/7/11 15:51
@@ -7,10 +11,15 @@ package xx.ms.design.partern.demo.proxy.JdkProxy;
  */
 public class JdkProxyMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Person person = (Person) new NationProxy().getInstance(new EveryBody());
         person.eat("apple");
         person.play();
+
+        byte[] bt = ProxyGenerator.generateProxyClass("proxy0", new Class[]{Person.class});
+        FileOutputStream fileOutputStream = new FileOutputStream("D://proxy0.class");
+        fileOutputStream.write(bt);
+        fileOutputStream.close();
     }
 
 }
